e7563e8fce2d4434f7e5c1c62d4d9e27ca144133
Ori Mizrahi <ori.mizrahi1000@gmail.com>
2026-04-18 15:05:00 +0300

    feat: implementation for a simple linked list and find_word

---
+++ src/dict.asm
@@ -0,0 +1,30 @@
+;; this is more like a linked list, not actually a dictionary
+  
+extern string_equals
+
+global find_word
+
+section .text
+
+;; rdi - pointer to a null terminated string - key, rsi - pointer to the last word in the dictionary
+find_word:
+.loop:
+  test rsi, rsi                 ; if rsi == 0
+  je .not_found                 ; we got to the end of the linked list, thus we didn't find the word
+
+  push rsi
+  lea rsi, [rsi + 8]            ; we need rsi to hold the key 
+  call string_equals
+  pop rsi
+
+  test rax, rax                 ; string_equals returns 1 if both strings are equal
+  jne .found
+
+  mov rsi, [rsi]                ; mov to the next node
+  jmp .loop
+.found:
+  mov rax, rsi
+  ret
+.not_found:
+  mov rax, 0
+  ret
---
+++ src/main.asm
@@ -0,0 +1,42 @@
+%include "src/colon.inc"
+
+global _start
+
+extern find_word
+
+extern exit
+extern print_string
+extern print_newline
+extern string_length
+
+section .rodata
+  colon "third word", third_word
+  db "third word explanation", 0
+  
+  colon "second word", second_word
+  db "second word explanation", 0
+  
+  colon "first word", first_word
+  db "first word explanation", 0
+
+section .data
+    my_word: db "first word", 0
+
+section .text
+_start:
+  mov rdi, my_word
+  mov rsi, first_word
+  call find_word
+  
+  test rax, rax
+  je .end
+
+  lea rdi, [rax + 8]
+  call string_length
+
+  lea rdi, [rdi + rax + 1]
+  call print_string
+  call print_newline
+.end:  
+  mov rdi, 0
+  call exit
